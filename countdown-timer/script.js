49ea67f7c4f3d50e101ca0cee889c746b7ea9823
anggadanangproject <124497491+anggadanangproject@users.noreply.github.com>
2023-02-07 12:14:16 +0700

    Add files via upload

---
+++ countdown-timer/script.js
@@ -0,0 +1,34 @@
+const newYears = '1 Jan 2024'
+const daysEl = document.getElementById('days')
+
+const hoursEl = document.getElementById('hours')
+
+const minsEl = document.getElementById('mins')
+
+const secondsEl = document.getElementById('seconds')
+
+
+function countdown() {
+	const newYearsDate = new Date(newYears)
+	const currentDate = new Date()
+
+	const diff = new Date(newYearsDate - currentDate)/1000
+	const days = Math.floor(diff/3600/24)
+	const hours = Math.floor(diff/3600)%24
+	const minutes = (Math.floor(diff/60)%60)
+	const seconds = Math.floor(diff)%60
+
+	console.log(days , hours , minutes , formatTime(seconds))
+	
+	daysEl.innerHTML = days
+	hoursEl.innerHTML = hours
+	minsEl.innerHTML = minutes
+	secondsEl.innerHTML = formatTime(seconds)
+}
+
+function formatTime(time) {
+
+	return time < 10 ? ("0" + time): time
+}
+countdown()
+setInterval(countdown, 1000)
